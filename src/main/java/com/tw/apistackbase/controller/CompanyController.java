be38722f857fde96bc40a64a3c12b39494a6ac41
LILY KAN (ITA-ISDC-ISD-OOCLL/ZHA) <KANLI2@OOCL.COM>
2019-07-17 00:28:35 +0800

    get:specific Employee

--- src/main/java/com/tw/apistackbase/controller/CompanyController.java
+++ src/main/java/com/tw/apistackbase/controller/CompanyController.java
@@ -3,11 +3,13 @@ package com.tw.apistackbase.controller;
 import com.tw.apistackbase.model.Company;
 import com.tw.apistackbase.model.Employees;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/company")
@@ -16,6 +18,11 @@ public class CompanyController {
     public List<Company> getEmployees() {
         List<Employees>employee=new ArrayList<>();
         return Company.createCompany();
+    }
+    @GetMapping("/{id}")
+    public Company getEmployees(@PathVariable int id) {
+        List<Company>complany=Company.createCompany();
+        return complany.stream().filter(e -> e.getId() == id).collect(Collectors.toList()).get(0);
     }
 
 }
--- src/main/java/com/tw/apistackbase/model/Company.java
+++ src/main/java/com/tw/apistackbase/model/Company.java
@@ -4,12 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Company {
+    private int id;
     private String companyName;
     private int employeesNumber;
     private List<Employees> employeesList;
 
     public String getCompanyName() {
         return companyName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public void setCompanyName(String companyName) {
@@ -32,15 +41,21 @@ public class Company {
         this.employeesList = employeesList;
     }
 
-    public Company(String companyName, int employeesNumber) {
+//    public Company(String companyName, int employeesNumber) {
+//        this.companyName = companyName;
+//        this.employeesNumber = employeesNumber;
+//    }
+
+    public Company(int id, String companyName, int employeesNumber) {
+        this.id = id;
         this.companyName = companyName;
         this.employeesNumber = employeesNumber;
     }
 
     public static List<Company> createCompany() {
         List<Company>company=new ArrayList<>();
-        company.add(new Company("baidu",10));
-        company.add(new Company("google",100));
+        company.add(new Company(1,"baidu",10));
+        company.add(new Company(2,"google",100));
 
         return  company;
 
